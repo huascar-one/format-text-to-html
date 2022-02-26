3c6c292717f86e0f1bc1859d74ade26f9b385b62
huascar-one <huascar360@gmail.com>
2022-02-26 11:38:32 -0400

    initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,32 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Anchor, Button, Container, Box, Center } from '@mantine/core';
+import { useBooleanToggle } from '@mantine/hooks';
+import RichTextEditor from '../test';
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { Prism } from '@mantine/prism';
 
 export default function HomePage() {
+  const [value, onChange] = useState('');
+  console.log(value);
   return (
     <>
       <Title sx={{ fontSize: 100, fontWeight: 900, letterSpacing: -2 }} align="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span">
-          Mantine
-        </Text>
+        Convert
       </Title>
-      <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-        This starter Next.js projects includes a minimal setup for server side rendering, if you
-        want to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/theming/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit index.tsx file.
-      </Text>
+      <Prism language="markup">{value}</Prism>
+      <br />
+      <Center>
+        <Button
+          variant="outline"
+          onClick={() => {
+            onChange('');
+          }}
+        >
+          Delete
+        </Button>
+      </Center>
+      <br />
+      <RichTextEditor value={value} onChange={onChange} />
     </>
   );
 }
